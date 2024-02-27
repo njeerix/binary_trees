ba7078fafba11ae57dfd3077731f69e446383b69
Michelle Njeeri <michellenjeeeri@gmail.com>
2024-02-27 00:06:19 -0800

    binary_trees

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -11,11 +11,25 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree)
 	{
 		size_t l = 0, r = 0;
-		
+
 		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 		return ((l > r) ? l : r);
 	}
+	return (0);
+}
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: A pointer to the root node of the tree to measure the size
+ *
+ * Return: The size of the tree or 0 if @tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree)
+		return (1 + binary_tree_size(tree->left) +
+			binary_tree_size(tree->right));
 	return (0);
 }
 
@@ -27,16 +41,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t l_height = 0, r_height = 0;
+	size_t size, height;
 
-	if (tree)
-	{
-	l_height = binary_tree_height(tree->left);
-	r_height = binary_tree_height(tree->right);
-	if (l_height != r_height)
+	if (!tree)
 		return (0);
-	if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
-		return (1);
-	}
-	return (0);
+
+	size = binary_tree_size(tree);
+	height = binary_tree_height(tree);
+
+	return (size == ((1 << (height + 1U)) - 1U));
 }
--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,7 +8,8 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL || node->parent->left == NULL || node->parent->right == NULL)
+	if (node == NULL || node->parent == NULL ||
+			node->parent->left == NULL || node->parent->right == NULL)
 		return (NULL);
 
 	if (node->parent->left == node)
