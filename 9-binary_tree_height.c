bd3e4596e7bd38663243d7b3675a40dbe090a057
Michelle Njeeri <michellenjeeeri@gmail.com>
2024-02-26 22:55:39 -0800

    binary_trees

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,13 +9,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree)
-	{
-		size_t l = 0; r = 0;
+	size_t left_height, right_height;
 
-		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		return ((l > r) ? l : r);
-	}
-	return (0);
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
 }
