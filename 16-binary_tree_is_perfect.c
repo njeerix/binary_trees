b81fbe5a3b969f90a344d47f2d04c1cb4d3bbe95
Michelle Njeeri <michellenjeeeri@gmail.com>
2024-02-26 23:39:52 -0800

    binary_trees

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,10 +8,15 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	return (1 + (binary_tree_height(tree->left), binary_tree_height(tree->right)
-		? binary_tree_height(tree->left) : binary_tree_height(tree->right)));
+	if (tree)
+	{
+		size_t l = 0, r = 0;
+		
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
+	}
+	return (0);
 }
 
 /**
@@ -22,14 +27,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int height = binary_tree_height(tree);
-	int size = binary_tree_size(tree);
+	size_t l_height = 0, r_height = 0;
 
-	if (size == 0 || height == 0)
+	if (tree)
+	{
+	l_height = binary_tree_height(tree->left);
+	r_height = binary_tree_height(tree->right);
+	if (l_height != r_height)
 		return (0);
-
-	if ((1 << height) - 1 == size)
+	if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
 		return (1);
-
+	}
 	return (0);
 }
