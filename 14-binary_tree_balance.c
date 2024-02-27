eb3d33a7c888e90fca9315227d4cd76f75b7a4f4
Michelle Njeeri <michellenjeeeri@gmail.com>
2024-02-27 00:24:00 -0800

    binary_trees

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -28,13 +28,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
-
 	if (tree == NULL)
 		return (0);
 
-	left_height = (int)binary_tree_height(tree->left);
-	right_height = (int)binary_tree_height(tree->right);
+	int left_height = (int)binary_tree_height(tree->left);
+	int right_height = (int)binary_tree_height(tree->right);
 
 	return (left_height - right_height);
 }
