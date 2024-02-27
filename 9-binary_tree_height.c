67c3d9d30f84c31a8e8624b1c18bd3d911e463ab
Michelle Njeeri <michellenjeeeri@gmail.com>
2024-02-26 22:50:04 -0800

    binary_trees

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,16 +9,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	if (tree)
+	{
+		size_t l = 0; r = 0;
 
-	if (tree == NULL)
-		return (0);
-
-	/* Compute the height of the left and right subtrees recursively */
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height > right_height)
-		return (left_height + 1);
-	return (right_height + 1);
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
+	}
+	return (0);
 }
